c9b7f30467e2a4e3b3ae97ee940cad8dfb09dd05
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-05-02 21:12:20 -0400

    score logic added

--- server.js
+++ server.js
@@ -7,36 +7,58 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000
 
+const friends = require('./app/data/friends.json');
+
 // Expose the public directory to access CSS
 app.use(express.static(path.join(__dirname, './app/public')));
 
 //middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-// Add the application routes
-require(path.join(__dirname, './apiRoutes'))(app);
-require(path.join(__dirname, './htmlRoutes'))(app);
+app.post('/compare', (request, response) => {
 
-// open PORT to start listening
-app.listen(PORT, function() {
-  console.log('server.js is on PORT: ' + PORT);
+  console.log(request.body);
+
+  response.status(200).send(compareScores(request.body.name, request.body.photo, request.body.scores));
+
 });
 
 
+// open PORT to start listening
+app.listen(PORT, function () {
 
+  console.log('server.js is on PORT: ' + PORT);
 
+});
 
+function compareScores(name, url, scoresArr) {
 
+  let friendsScores = [];
 
+  for (let i = 0; i < friends.length; i++) {
 
+    let sumOfDiffs = 0;
 
+    for (let j = 0; j < friends[i].scores.length; j++) {
 
+      sumOfDiffs += Math.abs(friends[i].scores[j] - scoresArr[j]);
 
+    }
 
+    friends[i].sumOfDiffs = sumOfDiffs;
 
+    console.log(`${friends[i].name} => ${sumOfDiffs}`);
 
+  }
 
+  return friends.reduce((prev, current) => {
 
+    return (prev.sumOfDiffs < current.sumOfDiffs) ? prev : current;
 
+  });
+
+}
